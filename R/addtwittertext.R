18ccaf018a53371e1243eeaa91b07f8e075c1b93
carlosyanez <carlos.yanez.s@gmail.com>
2021-04-17 14:46:28 +0200

    changes to social logo

--- R/addtwittertext.R
+++ R/addtwittertext.R
@@ -4,13 +4,13 @@
 #' @param   logo_size (default = 9)
 #' @param   logo_url custom image (i.e dark mode). If none provide, will download logo from wikimedia
 #' @importFrom stringr str_c
-#' @export add_twitter_text
-add_twitter_text <- function(twitter_handle, logo_size=9,logo_url=NULL){
+#' @export add_social_ref
+add_social_ref <- function(twitter_handle, logo_size=9,logo_url=NULL){
 
   if(is.null(logo_url)) logo_url <-"https://upload.wikimedia.org/wikipedia/de/thumb/9/9f/Twitter_bird_logo_2012.svg/200px-Twitter_bird_logo_2012.svg.png"
 
 
-  text <- str_c("<img src='https://upload.wikimedia.org/wikipedia/de/thumb/9/9f/Twitter_bird_logo_2012.svg/200px-Twitter_bird_logo_2012.svg.png' height='",
+  text <- str_c("<img src='",logo_url,"' height='",
                 logo_size,"' />",twitter_handle)
 
 

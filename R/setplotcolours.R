fd401f52af227f5d543c35d5617e68c6b520d787
carlosyanez <carlos.yanez.s@gmail.com>
2021-07-09 10:22:33 +0200

    default for horizontal and vertical lines

--- R/setplotcolours.R
+++ R/setplotcolours.R
@@ -6,8 +6,11 @@
 #' @param alpha_setting  alpha value when fill_colour is used
 #' @param alt_fill_colour fill colour for plots like boxplots
 #' @export set_plot_colours
-set_plot_colours <- function(line_colour="darkblue",fill_colour="deepskyblue",
+set_plot_colours <- function(line_colour="darkblue",
+                             fill_colour="deepskyblue",
+                             HVline_colour="orange",
                              alt_fill_colour="white",alpha_setting=0.3){
+
 
   ggplot2::update_geom_defaults("point",   list(colour = line_colour))
   ggplot2::update_geom_defaults("violin",   list(colour = line_colour,fill = fill_colour))
@@ -17,5 +20,7 @@ set_plot_colours <- function(line_colour="darkblue",fill_colour="deepskyblue",
   ggplot2::update_geom_defaults("boxplot",   list(colour = line_colour,fill = alt_fill_colour))
   ggplot2::update_geom_defaults("density",   list(colour = line_colour,fill = fill_colour,alpha=alpha_setting))
   ggplot2::update_geom_defaults("rect",   list(colour = line_colour,fill = fill_colour,alpha=alpha_setting))
+  ggplot2::update_geom_defaults("GeomHline",   list(colour = HVline_colour))
+  ggplot2::update_geom_defaults("GeomVline",   list(colour = HVline_colour))
 
 }
